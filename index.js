096529426b9b29972cd333ad531837b43102cf14
jishan <49402826+jishan484@users.noreply.github.com>
2019-12-11 18:13:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var port = process.env.PORT | 80 ;
 
 app.use('/',express.static(__dirname + '/public'))
 
@@ -7,6 +8,6 @@ app.get('/hi', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(process.env.PORT , function() {
+app.listen(port , function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
