b75ce86d0b0ebd4f5813ae4678564fba6f55f8f0
jishan <49402826+jishan484@users.noreply.github.com>
2022-04-10 21:57:17 +0530

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var port = process.env.PORT | 8080 ;
+var port = process.env.PORT;
 
 app.use('/',express.static(__dirname + '/public'))
 
@@ -9,5 +9,5 @@ app.get('/', function(request, response) {
 })
 
 app.listen(port , function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + port);
 })
